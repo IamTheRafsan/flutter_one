2999d096875a6ae9f4caff47cc8030364ef89dbb
IamTheRafsane <sending.to.evan@gmail.com>
2024-01-09 20:05:09 +0600

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("This is my first"),
+    home: Scaffold(
+      backgroundColor: Colors.blue[600],
+      body: Container(
+        child: Center(
+          child: Image(
+            image: NetworkImage("https://images.indianexpress.com/2023/10/SHAKIB-AL-HASAN-PTI-CROP.jpg"),
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("App Bar"),
+        backgroundColor: Colors.blue[900],
+      ),
     ),
   ));
 }
-
-
